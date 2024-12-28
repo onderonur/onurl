db2eb41bdd0e96c78ba9cb02f77f0a2e01085c18
onderonur <onuronder.onr@gmail.com>
2024-12-28 17:55:48 +0300

    minor aria improvement

--- src/core/forms/components/form-control.tsx
+++ src/core/forms/components/form-control.tsx
@@ -69,11 +69,13 @@ export function FormErrorMessage() {
 
   if (!errorMessages?.length) return null;
 
-  // TODO: Will check example aria attributes and html tags etc.
-  // And will apply to all form items.
-  // https://chakra-ui.com/docs/components/form-control#improvements-from-v1
   return (
-    <div id={ids.message} className="text-error-600" aria-live="polite">
+    <div
+      id={ids.message}
+      className="text-error-600"
+      aria-live="polite"
+      aria-atomic
+    >
       {errorMessages.join(', ')}
     </div>
   );
